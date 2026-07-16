db64a4a2ee50497bfa5ddb59292185ac17d93cc3
Aniket Patel <mainavvideo@gmail.com>
2026-07-16 20:52:09 +0530

    Describe your changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,63 +1,60 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./Pages/App.jsx";
 import Home from "./Pages/Home.jsx";
 import ActressPage from "./Pages/actress.jsx";
-import StudioPage from "./Pages/studio.jsx";
 import WatchPage from "./Pages/watch.jsx";
 import Login from "./Pages/loginpage.jsx";
 import ProtectedRoute from "./component/protectrout/protectrout.jsx";
 import SearchPage from "./Pages/search.jsx";
-import Trending from "./Pages/trending.jsx";
 import Upload from "./Pages/upload.jsx";
+import SeriesPage from "./Pages/searies.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <App />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-       {
-        path: "/trending",
-        element: <Trending />,
-      },
-      {
-        path: "watch/:id",
-        element: <WatchPage />,
-      },
-      {
-        path: "studio/:name",
-        element: <StudioPage />,
-      },
-      {
-        path: "actress/:name",
-        element: <ActressPage />,
-      },
-      {
-        path: "search/:keyword",
-        element: <SearchPage />,
-      },
-      {
-        path: "/upload",
-        element: <Upload />,
-      },
-    ],
-  },
-]);
+const router = createHashRouter(
+  [
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <App />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "watch/:id",
+          element: <WatchPage />,
+        },
+        {
+          path: "series/:name",
+          element: <SeriesPage />,
+        },
+        {
+          path: "actress/:name",
+          element: <ActressPage />,
+        },
+        {
+          path: "search/:keyword",
+          element: <SearchPage />,
+        },
+        {
+          path: "upload",
+          element: <Upload />,
+        },
+      ],
+    },
+  ],
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
